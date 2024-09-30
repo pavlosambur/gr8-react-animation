ae7b93cb659c06de8b8d3d77cee218a283bcd0c0
Pavlo <samburp@gmail.com>
2024-09-30 21:39:54 +0300

    FavoriteIconLeague

--- app/src/components/AllLive.tsx
+++ app/src/components/AllLive.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import { iso31661 } from "iso-3166";
+import Cookies from "js-cookie";
 
 function getCountryCode(countryName: string) {
     const country = iso31661.find(
@@ -11,20 +12,54 @@ function getCountryCode(countryName: string) {
 
 // https://megapari.com/service-api/LiveFeed/Get1x2_VZip?sports=1&count=1000&gr=824&mode=4&country=2&partner=192&getEmpty=true&virtualSports=true&countryFirst=true&noFilterBlockEvent=true
 
-const FavoriteIconLeague: React.FC<{ size: number }> = ({ size }) => {
+const FavoriteIconLeague: React.FC<{ size: number; leagueId: string }> = ({
+    size,
+    leagueId,
+}) => {
+    const [isClicked, setIsClicked] = useState(() => {
+        const savedState = Cookies.get(`favoriteIconState-${leagueId}`);
+        return savedState ? JSON.parse(savedState) : false; // Загружаем состояние из куки или начальное false
+    });
+    const [disableHover, setDisableHover] = useState(false);
+
+    const handleClick = () => {
+        const newState = !isClicked;
+        setIsClicked(newState); // Меняем состояние при клике
+        Cookies.set(`favoriteIconState-${leagueId}`, JSON.stringify(newState), {
+            expires: 365,
+        }); // Сохраняем состояние в куки
+
+        setDisableHover(true); // Отключаем ховер после клика
+        setTimeout(() => {
+            setDisableHover(false); // Включаем ховер через 300ms
+        }, 700);
+    };
+
     return (
-        <div className="flex w-4 h-4">
+        <div className={`flex w-${size} h-${size} group`} onClick={handleClick}>
             <span>
                 <svg
-                    className={`w-${size} h-${size}`}
+                    className={`w-${size} h-${size} transition-colors duration-300 ${
+                        isClicked
+                            ? `fill-[var(--icon-hover-v1)] ${
+                                  disableHover
+                                      ? ""
+                                      : "group-hover:fill-[var(--icon-main-v1)]"
+                              }`
+                            : `fill-[var(--icon-main-v1)] ${
+                                  disableHover
+                                      ? ""
+                                      : "group-hover:fill-[var(--icon-hover-v1)]"
+                              }`
+                    }`}
                     viewBox="0 0 24 24"
-                    fill="none"
                     xmlns="http://www.w3.org/2000/svg"
                 >
-                    <path
-                        d="M11.9499 6.1L13.5526 10.3H18.0601L14.5543 13.3L15.8564 17.6L11.9499 15.1L8.04341 17.6L9.34558 13.3L5.83974 10.3H10.3472L11.9499 6.1ZM11.9499 2C11.0484 2 10.7479 4 8.94491 8.3H3.43573C1.83306 8.3 1.53256 9.2 2.73456 10.2L7.04174 14C6.14024 17.6 4.23707 21 5.9399 21C6.74124 21 7.24207 20.4 11.9499 17.5C16.6578 20.4 17.1586 21 17.9599 21C19.6628 21 17.7596 17.6 16.8581 14L21.2654 10.2C22.4674 9.2 22.1669 8.3 20.5643 8.3H14.9549C13.1519 4.1 12.9516 2 11.9499 2Z"
-                        fill="var(--icon-main)"
-                    />
+                    {isClicked ? (
+                        <path d="M11.9499 2C11.0484 2 10.7479 4 8.94491 8.3H3.43573C1.83306 8.3 1.53256 9.2 2.73456 10.2L7.04174 14C6.14024 17.6 4.23707 21 5.9399 21C6.74124 21 7.24207 20.4 11.9499 17.5C16.6578 20.4 17.1586 21 17.9599 21C19.6628 21 17.7596 17.6 16.8581 14L21.2654 10.2C22.4674 9.2 22.1669 8.3 20.5643 8.3H14.9549C13.1519 4.1 12.9516 2 11.9499 2Z" />
+                    ) : (
+                        <path d="M11.9499 6.1L13.5526 10.3H18.0601L14.5543 13.3L15.8564 17.6L11.9499 15.1L8.04341 17.6L9.34558 13.3L5.83974 10.3H10.3472L11.9499 6.1ZM11.9499 2C11.0484 2 10.7479 4 8.94491 8.3H3.43573C1.83306 8.3 1.53256 9.2 2.73456 10.2L7.04174 14C6.14024 17.6 4.23707 21 5.9399 21C6.74124 21 7.24207 20.4 11.9499 17.5C16.6578 20.4 17.1586 21 17.9599 21C19.6628 21 17.7596 17.6 16.8581 14L21.2654 10.2C22.4674 9.2 22.1669 8.3 20.5643 8.3H14.9549C13.1519 4.1 12.9516 2 11.9499 2Z" />
+                    )}
                 </svg>
             </span>
         </div>
@@ -300,7 +335,10 @@ const AllLive: React.FC = () => {
                                         leagueName={matches[0].LE} // Берем название лиги из первого матча
                                     />
                                 </div>
-                                <FavoriteIconLeague size={4} />
+                                <FavoriteIconLeague
+                                    size={4}
+                                    leagueId={matches[0].LI}
+                                />
                             </div>
 
                             {/* Рендерим матчи внутри лиги */}
